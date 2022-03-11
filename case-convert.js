2b1eae138ae0903e7e04169d0fb18b9b3bdb9a20
asraful7711 <asraful7711@gmail.com>
2022-03-12 03:47:45 +0600

    all the strings array concepts commited

---
+++ case-convert.js
@@ -0,0 +1,19 @@
+const anthem = 'amar shonar bangla ami tomai valobashi'
+
+// toLocaleLowerCase
+// toLocaleLowerCase er shahajje shobgulo case ke lover case bananano hoi,fole compare korar shomoi case sensetive jhamela hoi na 
+const userInput = 'kalachaNpaKhi'
+const userName = 'kalachanpakhI'
+if (userInput.toLocaleLowerCase() == userName.toLocaleLowerCase()){
+    // console.log('kalachan exists')
+}
+
+
+// touppercase
+// toUpperCase shobgulo string er vetorer okkhor gulo k boro hater kore dei. 
+const shuyaChanInput = 'shuyaChanPAkhi'
+const shuyaChanName = 'shuyachanpakhi'
+if (userInput.toUpperCase() == userName.toUpperCase()){
+    console.log('shuyachan exists')
+}
+
